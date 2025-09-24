0b6a44516c7413069fe9556d8bccc6e07fa5a2d6
pratikxsurf <pratik@surf.mt>
2025-09-23 18:24:19 -0700

    comment out phone number and otp verification screen for new flow

--- src/navigation/stacks/AuthNavigator.tsx
+++ src/navigation/stacks/AuthNavigator.tsx
@@ -1,9 +1,9 @@
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import AuthSuccessScreen from '../../screens/Auth/AuthSuccessScreen/AuthSuccessScreen';
 import OTPVerificationScreen from '../../screens/Auth/OTPVerificationScreen/OTPVerificationScreen';
 import PhoneNumberScreen from '../../screens/Auth/PhoneNumberScreen/PhoneNumberScreen';
-import {AuthStackParamList} from '../../types/navigation';
+import { AuthStackParamList } from '../../types/navigation';
 import EmailSignIn from '../../screens/Auth/EmailSignInPages/EmailSignIn';
 
 const Stack = createStackNavigator<AuthStackParamList>();
@@ -14,9 +14,9 @@ export const AuthNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="PhoneNumber" component={PhoneNumberScreen} />
       <Stack.Screen name="EmailSignIn" component={EmailSignIn} />
-      <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} />
+      {/* <Stack.Screen name="PhoneNumber" component={PhoneNumberScreen} /> */}
+      {/* <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} /> */}
       <Stack.Screen name="AuthSuccess" component={AuthSuccessScreen} />
     </Stack.Navigator>
   );
