4a193fa18e70b208435ed9e9a57b29ff12e60608
pratikxsurf <pratik@teams.surf.mt>
2025-10-02 18:56:16 +0530

    changes

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,20 +1,20 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {checkAuthStatus} from '../redux/slices/authSlice';
-import type {AppDispatch, RootState} from '../redux/store';
-import {RootStackParamList} from '../types/navigation';
-import {AuthNavigator} from './stacks/AuthNavigator';
-import {CreateNavigator} from './stacks/CreateNavigator';
-import {DashboardNavigator} from './stacks/DashBoardNavigator';
-import {OnboardingNavigator} from './stacks/OnboardingNavigator';
-import {VATNavigator} from './stacks/VATNavigator';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkAuthStatus } from '../redux/slices/authSlice';
+import type { AppDispatch, RootState } from '../redux/store';
+import { RootStackParamList } from '../types/navigation';
+import { AuthNavigator } from './stacks/AuthNavigator';
+import { CreateNavigator } from './stacks/CreateNavigator';
+import { DashboardNavigator } from './stacks/DashBoardNavigator';
+import { OnboardingNavigator } from './stacks/OnboardingNavigator';
+import { VATNavigator } from './stacks/VATNavigator';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const {isLoggedIn, isLoading} = useSelector((state: RootState) => state.auth);
+  const { isLoggedIn, isLoading } = useSelector((state: RootState) => state.auth);
   const [isInitialized, setIsInitialized] = useState(false);
 
   // Check auth status when app starts
