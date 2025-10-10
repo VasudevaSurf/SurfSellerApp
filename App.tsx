dfe5bf2cf095b22fddbc5e67dc6364a8bf0dbffd
pratikxsurf <pratik@teams.surf.mt>
2025-10-10 16:20:28 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -5,13 +5,12 @@ import { StatusBar } from 'react-native';
 import store from './src/redux/store';
 import { RootNavigator } from './src/navigation/RootNavigator';
 import { navigationRef } from './src/navigation/utils/navigationRef';
-import Toast, { ToastConfig } from 'react-native-toast-message';
+import Toast from 'react-native-toast-message';
 
 import { ToastComponent } from './src/components/MainComponents/Toast/ToastComponent';
-const toastConfig: ToastConfig = {
-  success: (props) => <ToastComponent {...props} />,
-  error: (props) => <ToastComponent {...props} />,
-  info: (props) => <ToastComponent {...props} />,
+const toastConfig = {
+  success: (props: any) => <ToastComponent {...props} />,
+  error: (props: any) => <ToastComponent {...props} />,
 };
 
 
@@ -22,7 +21,6 @@ const App = () => {
       <StatusBar backgroundColor="white" barStyle="dark-content" />
       <NavigationContainer ref={navigationRef}>
         <RootNavigator />
-        {/* <Toast ref={(ref) => Toast.setRef(ref)} /> */}
         <Toast config={toastConfig} ref={(ref) => Toast.setRef(ref)} />
       </NavigationContainer>
     </Provider>
